7b53ef6f1c7cadf8d66df3d914bd713e4f8320e3
Andy Grindstaff <andygrindstaff@gmail.com>
2017-03-22 21:15:40 +0000

    Added plans table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322143207) do
+ActiveRecord::Schema.define(version: 20170322210448) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -20,8 +20,16 @@ ActiveRecord::Schema.define(version: 20170322143207) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "plans", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
+    t.string   "string",                 default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
